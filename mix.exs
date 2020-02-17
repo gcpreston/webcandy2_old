cded02a80a95abade8ad5973780d30c5d5ffc04e
Graham Preston <graham.preston@gmail.com>
2020-02-17 16:04:48 -0500

    Add Guardian

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule Webcandy2.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:pbkdf2_elixir, "~> 1.0"}
+      {:pbkdf2_elixir, "~> 1.0"},
+      {:guardian, "~> 2.0.0"}
     ]
   end
 
