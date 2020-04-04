15ae3fbe2dd45fe06d350cc4b57529bca94a69e5
Graham Preston <graham.preston@gmail.com>
2020-04-03 20:48:45 -0400

    Implement signup form functionality

--- assets/js/SignupForm.jsx
+++ assets/js/SignupForm.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { Box, Button, TextField, Card } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,18 +24,41 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-export default function SignupForm() {
+export default function SignupForm(props) {
   const classes = useStyles();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [toHome, setToHome] = useState(false);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    axios.post('/api/v1/user', {
+      'user': {
+        'email': email,
+        'password': password,
+        'confirm_password': confirmPassword
+      }
+     }).then((response) => {
+      alert('User created successfully!');
+      localStorage.setItem('token', response.data['token']);
+      localStorage.setItem('renewal_token', response.data['renewal_token']);
+      setToHome(true);
+    }).catch((error) => {
+      console.log(error);
+      alert('Error creating user.');
+    });
+  }
 
   return (
     <React.Fragment>
+      {toHome ? <Redirect to="/" /> : null}
+
       <Card className={classes.mainCard}>
         <h2>Create an Account</h2>
-        <form className={classes.root}>
+        <form className={classes.root} onSubmit={handleSubmit}>
           <Box display="flex" flexDirection="column">
             <TextField
               required
